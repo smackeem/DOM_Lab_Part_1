32af743005cd6be626016ecfe4ecc6150a1c45d7
smackeem <smackeem@outlook.com>
2023-10-25 12:16:45 -0400

    Added event listener to top menu elements.

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Menu data structure
 const menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 const mainEl = document.querySelector('main');
 mainEl.style.backgroundColor = 'var(--main-bg)';
@@ -22,10 +33,26 @@ menuLinks.forEach(function(menuLink){
     linkEl.innerText = menuLink.text;
     topMenuEl.appendChild(linkEl);
 })
-console.log(topMenuEl)
-
+// console.log(topMenuEl)
+// Sub Menu Elements
 const subMenuEl = document.getElementById('sub-menu');
-/* console.log(subMenuEl) */
+// console.log(subMenuEl)
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.setAttribute("class","flex-around");
+subMenuEl.style.position = 'absolute';
+subMenuEl.style.top = '0';
+
+const topMenuLinks = topMenuEl.querySelectorAll('a');
+console.log(topMenuLinks)
+let showingSubMenu = false;
+
+topMenuEl.addEventListener('click',function(event){
+  event.preventDefault();
+  if(event.target.tagName !== 'A'){
+    return;
+  }else{
+
+    console.log(event.target.innerHTML);
+  }
+})
